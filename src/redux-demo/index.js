7c26dbecc4157ad932b1347aa5acfea067bc794f
siva krishnan <sivakrishnancse052@gmail.com>
2025-07-09 06:45:23 +0530

    [Skrish] upto video 6 reducer

---
+++ src/redux-demo/index.js
@@ -0,0 +1,22 @@
+const BUY_CAKE = 'BUY_CAKE'
+
+function buyCake() {
+    return {
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
+
+const initialState = {
+    numOfCake: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCake: state.numOfCake - 1
+        }
+        default: return state
+    }
+}
